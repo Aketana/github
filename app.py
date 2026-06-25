988dfbdaa1afa7ed647ef59e627c8bf1ab4b7eae
เอกธนา รัตนาวังเจริญ <AketanaR@bot.or.th>
2026-06-25 10:11:36 +0700

    edit .yml

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("Hello from Docker Container")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🚀 GitHub Actions Lab</h1>
+    <h2>Hello from Flask running in Docker</h2>
+    <p>Mission 2 Complete</p>
+    """
+
+@app.route("/health")
+def health():
+    return {
+        "status": "OK",
+        "version": "1.0",
+        "message": "Application is running"
+    }
+
+@app.route("/about")
+def about():
+    return {
+        "author": "Aketana",
+        "lab": "GitHub Actions + Docker"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
